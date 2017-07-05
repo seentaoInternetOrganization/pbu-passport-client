322bbcc90366c37563ba9d5d7ce29d385cdc6e81
AngusChen <angus1105@gmail.com>
2017-07-05 18:24:29 +0800

    bug fixed

--- index.js
+++ index.js
@@ -72,77 +72,44 @@ function getSidByTicket(req, res, config) {
     });
 }
 
-/**
- * 校验sid
- * @param  {object} req    请求体
- * @param  {object} res    响应体
- * @param  {object} config 配置文件
- * @return {Promise}        Promise
- */
-function checkSid(req, res, config) {
-    return new Promise(function(resolve, reject) {
-        request.post({
-            url: urljoin(config.passportUrl, 'sid.check'),
-            form: {
-                userToken: req.cookies[md5('userToken')],
-                userId: req.cookies[md5('userId')],
-                userName: req.cookies[md5('userName')],
-                userType: req.cookies[md5('userType')],
-                sid: req.cookies.PBUSID
-            }
-        }, function(err, response, body) {
-            if (err) {
-                reject(err);
-            }
-
-            const ret = JSON.parse(body);
-            resolve(ret);
-        })
-    });
-}
-
 module.exports.pbupassport = pbupassport;
 function pbupassport(config) {
     return function(req, res, next) {
         if (req.method === 'GET' ) {
-            if (!req.cookies.PBUSID) {
-                if (!req.query.ticket) {
+
+            if (!req.query.ticket) {
+                if (!req.cookies.PBUSID) {
+                    //没票且没sid，则重新登录
                     res.redirect(appendQuery(config.passportUrl, { redirectUrl:  urljoin(config.siteDomain, req.originalUrl) }));
                     return;
                 }else {
-                    const maxAge = config.maxAge;
-                    //用ticket换sid
-                    getSidByTicket(req, res, config).then(function(ret) {
-                        res.cookie(PBUSID, ret.sid, { maxAge: maxAge, httpOnly: true });
-                        res.cookie(md5('userName'), base64Encode(ret.userName), { maxAge: maxAge });
-                        res.cookie(md5('userId'), base64Encode(ret.userName), { maxAge: maxAge });
-                        res.cookie(md5('userToken'), base64Encode(ret.userName), { maxAge: maxAge });
-                        res.cookie(md5('userType'), base64Encode(ret.userName), { maxAge: maxAge });
-                        res.redirect(appendQuery(req.originalUrl, { ticket: null }, { removeNull: true }));
-                        return;
-                    });
 
-                    return;
+                    if (req.cookies[md5('userToken')]
+                        && req.cookies[md5('userId')]
+                        && req.cookies[md5('userName')]
+                        && req.cookies[md5('userType')]) {
+                        //如果有sid且有userInfo等信息，则继续
+                    }else {
+                        //userInfo信息不全，需重新登录
+                        res.redirect(appendQuery(urljoin(config.passportUrl, 'login'), { redirectUrl:  urljoin(config.siteDomain, req.originalUrl) }));
+                        return;
+                    }
                 }
             }else {
-                //如果有userInfo等信息，则继续
-                if (req.cookies[md5('userToken')]
-                    && req.cookies[md5('userId')]
-                    && req.cookies[md5('userName')]
-                    && req.cookies[md5('userType')]) {
-
-                    checkSid(req, res, config).then(function(ret) {
-                        if (ret.same !== 'true') {
-                            res.redirect(appendQuery(urljoin(config.passportUrl, 'login'), { redirectUrl:  urljoin(config.siteDomain, req.originalUrl) }));
-                            return;
-                        }else {
-                            return next();
-                        }
-                    });
-                }else {
-                    res.redirect(appendQuery(urljoin(config.passportUrl, 'login'), { redirectUrl:  urljoin(config.siteDomain, req.originalUrl) }));
+                //只要有新票，就重新创建sid等信息
+                const maxAge = config.maxAge;
+                //用ticket换sid
+                getSidByTicket(req, res, config).then(function(ret) {
+                    res.cookie(PBUSID, ret.sid, { maxAge: maxAge, httpOnly: true });
+                    res.cookie(md5('userName'), base64Encode(ret.userName), { maxAge: maxAge });
+                    res.cookie(md5('userId'), base64Encode(ret.userName), { maxAge: maxAge });
+                    res.cookie(md5('userToken'), base64Encode(ret.userName), { maxAge: maxAge });
+                    res.cookie(md5('userType'), base64Encode(ret.userName), { maxAge: maxAge });
+                    res.redirect(appendQuery(req.originalUrl, { ticket: null }, { removeNull: true }));
                     return;
-                }
+                });
+
+                return;
             }
         }
 
